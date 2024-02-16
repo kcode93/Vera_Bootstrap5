9668c1e2956859af7691a99a6372eae4590af7d6
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-02-16 13:17:01 -0800

    if applied, the Replacement.js functionality to the Header

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -1,1 +1,12 @@
-//Custom Scripts HERE...
+//Replace Text in Header
+const checkReplace = document.querySelector('.replace-me');
+
+if(checkReplace !== null){
+    const replace = new ReplaceMe(checkReplace, {
+        animation: 'animated fadIn',
+        speed: 2000,
+        separator: ',',
+        loopCount: 'infinite',
+        autoRun: true
+    });
+}
