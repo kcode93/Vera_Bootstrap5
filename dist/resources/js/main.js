952da0068ab1ee6579edd41a4ea314af66c7df15
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-02-20 09:28:54 -0800

    if applied, this commit adds the dynamic styles for nav on scroll

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -10,3 +10,30 @@ if(checkReplace !== null){
         autoRun: true
     });
 }
+
+//adds style to navbar after nav at a certain position on screen
+function navStyleOnScroll(){
+    //targets HTML element
+    const navbar = document.querySelector('#main-nav');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50){
+            //adds background & shadow
+            //navbar.classList.add('bg-dark');
+            navbar.classList.add('border-bottom');
+            navbar.classList.add('border-primary');
+            navbar.classList.add('fixed-top');
+
+            
+        }else{
+            //removes background & shadow
+            //navbar.classList.remove('bg-dark');
+            navbar.classList.remove('border-bottom');
+            navbar.classList.remove('border-primary');
+            navbar.classList.remove('fixed-top');
+        }
+    });
+}
+
+//runs function after the page has loaded
+document.addEventListener('DOMContentLoaded', navStyleOnScroll);
